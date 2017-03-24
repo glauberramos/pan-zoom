fc7a54be274aed08c7b039a1519a0cdf1fc70502
Glauber Ramos <glauberamos@gmail.com>
2017-03-24 18:55:37 -0300

    Add slide

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(function () {
-  $('img').on('mousemove', function(event) {
+  $('.slide').click(function() {
+    $('img').addClass('is-active').addClass('is-visible');
+  });
+
+  $('.zoomable').click(function() {
+    $(this).removeClass('is-visible').removeClass('is-active');
+  });
+
+  $('.zoomable').on('mousemove', function(event) {
     $(this).css({'transform': 'translate3D(' + (-event.pageX) +  'px,' + (-event.pageY) + 'px' + ', 0px)'});
-  })
-})
+  });
+
+  $('.zoomable').on('touchmove', function(event) {
+    $(this).css({'transform': 'translate3D(' + (-event.pageX) +  'px,' + (-event.pageY) + 'px' + ', 0px)'});
+  });
+});
