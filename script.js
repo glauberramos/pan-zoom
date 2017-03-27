adf80d96bf1876c0672066c5626c9c7b2fac204c
Glauber Ramos <glauberamos@gmail.com>
2017-03-27 15:53:09 -0300

    Remove not used if

--- script.js
+++ script.js
@@ -23,29 +23,27 @@ function loadScroller() {
   window.addEventListener("resize", reflow, false);
   reflow();
 
-  if ('ontouchstart' in window) {
-    container.addEventListener("touchstart", function(e) {
-      // Don't react if initial down happens on a form element
-      if (e.touches[0] && e.touches[0].target && e.touches[0].target.tagName.match(/input|textarea|select/i)) {
-        return;
-      }
+  container.addEventListener("touchstart", function(e) {
+    // Don't react if initial down happens on a form element
+    if (e.touches[0] && e.touches[0].target && e.touches[0].target.tagName.match(/input|textarea|select/i)) {
+      return;
+    }
 
-      scroller.doTouchStart(e.touches, e.timeStamp);
-      e.preventDefault();
-    }, false);
+    scroller.doTouchStart(e.touches, e.timeStamp);
+    e.preventDefault();
+  }, false);
 
-    document.addEventListener("touchmove", function(e) {
-      scroller.doTouchMove(e.touches, e.timeStamp, e.scale);
-    }, false);
+  document.addEventListener("touchmove", function(e) {
+    scroller.doTouchMove(e.touches, e.timeStamp, e.scale);
+  }, false);
 
-    document.addEventListener("touchend", function(e) {
-      scroller.doTouchEnd(e.timeStamp);
-    }, false);
+  document.addEventListener("touchend", function(e) {
+    scroller.doTouchEnd(e.timeStamp);
+  }, false);
 
-    document.addEventListener("touchcancel", function(e) {
-      scroller.doTouchEnd(e.timeStamp);
-    }, false);
-  }
+  document.addEventListener("touchcancel", function(e) {
+    scroller.doTouchEnd(e.timeStamp);
+  }, false);
 }
 
 $(function () {
